36cbc02e6c8421a1399ced27c5528d71e9402508
JJMR2026 <jordanrohel@yahoo.ca>
2026-07-13 08:08:35 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,38 @@
-const CACHE_NAME = 'golf-tracker-v3';
-const urlsToCache = [ './', './index.html', './manifest.json' ];
+const CACHE_NAME = 'golf-tracker-pro-v3';
+
+// The files we want to cache initially
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
 
 self.addEventListener('install', event => {
-    self.skipWaiting();
-    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', event => {
-    event.waitUntil(caches.keys().then(cacheNames => Promise.all(cacheNames.map(cache => { if (cache !== CACHE_NAME) return caches.delete(cache); }))));
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
-// NETWORK FIRST STRATEGY
+// NETWORK-FIRST STRATEGY
+// It always tries to fetch fresh code from GitHub.
+// If the phone is completely offline, it falls back to the cache.
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
-    );
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
